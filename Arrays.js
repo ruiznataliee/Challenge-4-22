0f86ce83986d27d2332c427cc2e3b62192691fa2
natalieruiz <natalie.r0821@gmail.com>
2022-04-22 13:40:21 -0400

    pushing it up

---
+++ Arrays.js
@@ -0,0 +1,7 @@
+let students = ["Mia", "John", "Natalie", "James", "Stacy"]
+let ages = ["16", "17", "18", "19", "20"]
+let booleans = ["6>2", "3<9", "10=10", "12<20", "3=3"]
+
+function apply(students, ages, booleans) {
+
+}
